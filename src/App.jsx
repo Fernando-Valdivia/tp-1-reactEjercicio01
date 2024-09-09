1a51bf2e5f4ab46cd0753bee87829869ac1af8ea
Fernando Valdivia <fernandovaldivia92@gmail.com>
2024-09-09 10:44:36 -0300

    ejercicio 01

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <section>
+      <h1 className='text-center'>hola mundo</h1>
+      </section>
+  );
+};
+
+
+export default App;
